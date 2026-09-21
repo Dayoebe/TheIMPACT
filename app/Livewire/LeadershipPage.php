eb1cb7939afcdb162853267a6a2aabfd6710c0a3
Dayoebe <oyetoke.ebenezer@gmail.com>
2026-09-21 09:27:40 +0100

    Add LeadershipPage component and corresponding Blade view

---
+++ app/Livewire/LeadershipPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class LeadershipPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.leadership-page');
+    }
+}
